ac18b914cd085eb901720478ed7e7c381ea94c53
petrichor <david.j.chester.745125@gmail.com>
2023-10-01 20:30:34 +0900

    Updated to include a map generation page

--- location_mapper/script.js
+++ location_mapper/script.js
@@ -45,6 +45,26 @@ function removePOI() {
     }
 }
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    let modal = document.getElementById('myModal');
+    let span = document.getElementsByClassName('close')[0];
+
+    // Open the modal
+    modal.style.display = 'block';
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = 'none';
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    }
+});
+
 // function exportJSON() {
 //     const blob = new Blob([JSON.stringify(data, null, 4)], { type: 'application/json' });
 //     const a = document.createElement('a');
